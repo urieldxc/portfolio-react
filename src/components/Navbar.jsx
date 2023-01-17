59291b7bcbb106aeb43d1a3d42271edf900e24a8
Sergio Tomas <sergiota.cs@gmail.com>
2023-01-17 01:52:04 +0100

    mini-responsive and burguer menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import en from '../languages/en'
 import es from '../languages/es'
+import { Squeeze as Hamburger } from 'hamburger-react'
 
-function Navbar({lang, setLang}) {
+function Navbar({ lang, setLang }) {
+    const [isOpen, setOpen] = useState(false)
 
     const changeLang = (lang) => {
         setLang(lang)
     }
+
+    const handleToggle = () => {
+        document.querySelector(".menuLinks").classList.toggle("display")
+    }
     const t = lang === "en" ? en : es
-  return (
-    <>
-    <header>
-        <div className='navbar'>
-            <a href='/' className='logo'>ST</a>
-            <div className='navLinks'>
+    return (
+        <>
+            <header>
+                <div className='navbar'>
+                    <a href='/' className='logo'>ST</a>
+                    <div className='navLinks'>
+                        <ul>
+                            <li> <a href='#workDiv'>{t.work}</a></li>
+                            <li>{t.contact}</li>
+                            <li>{t.resumee}</li>
+                        </ul>
+                    </div>
+
+                    <div className='navLang'>
+                        <button onClick={() => changeLang("en")} className='buttonEN'>EN</button>
+                        |
+                        <button onClick={() => changeLang("es")} className='buttonES'>ES</button>
+                    </div>
+
+
+
+                    <Hamburger classList="hamburguerIcon" toggled={isOpen} toggle={setOpen} onToggle={toggled => {
+                        if (toggled) {
+                            handleToggle()
+                        } else {
+                            handleToggle()
+                        }
+                    }} />
+                </div>
+            </header>
+
+            <div className='menuLinks display'>
                 <ul>
-                    <li> <a href='#work'>{t.work}</a></li>
+                    <li> <a href='#workDiv'>{t.work}</a></li>
                     <li>{t.contact}</li>
                     <li>{t.resumee}</li>
                 </ul>
             </div>
-
-            <div className='navLang'>
-                <button onClick={() => changeLang("en") } className='buttonEN'>EN</button>
-                 | 
-                 <button onClick={() => changeLang("es") } className='buttonES'>ES</button>
-            </div>
-        </div>
-    </header>
-    </>
-  )
+        </>
+    )
 }
 
 export default Navbar
--- src/works.js
+++ src/works.js
@@ -34,33 +34,33 @@ export const workList = [
 
 
 
-    // {
-    //     title: "Portfolio",
-    //     descriptionEN: "Portfolio made with React",
-    //     descriptionES: "Portfolio hecho con React",
-    //     image: placeholder,
-    //     live: "/",
-    //     github: "/",
-    //     tags: ["HTML & CSS", "React"]
-    // },
-    // {
-    //     title: "Post-it App",
-    //     descriptionEN: "Post-it app made with vanilla JavaScript and TailwindCSS",
-    //     descriptionES: "Aplicación de post-it hecha con JavaScript puro y TailwindCSS",
-    //     image: placeholder,
-    //     live: "/",
-    //     github: "/",
-    //     tags: ["HTML & CSS", "JavaScript"]
-    // },
-    // {
-    //     title: "Apuri App",
-    //     descriptionEN: "Anime app made with React where you can have a personal list of your favourite animes",
-    //     descriptionES: "Aplicación de anime hecha con React en la cual puedes tener una lista personal de tus animes favoritos",
-    //     image: placeholder,
-    //     live: "/",
-    //     github: "/",
-    //     tags: ["HTML & CSS", "React"]
-    // },
+    {
+        title: "Portfolio",
+        descriptionEN: "Portfolio made with React",
+        descriptionES: "Portfolio hecho con React",
+        image: placeholder,
+        live: "/",
+        github: "/",
+        tags: ["HTML & CSS", "React"]
+    },
+    {
+        title: "Post-it App",
+        descriptionEN: "Post-it app made with vanilla JavaScript and TailwindCSS",
+        descriptionES: "Aplicación de post-it hecha con JavaScript puro y TailwindCSS",
+        image: placeholder,
+        live: "/",
+        github: "/",
+        tags: ["HTML & CSS", "JavaScript"]
+    },
+    {
+        title: "Apuri App",
+        descriptionEN: "Anime app made with React where you can have a personal list of your favourite animes",
+        descriptionES: "Aplicación de anime hecha con React en la cual puedes tener una lista personal de tus animes favoritos",
+        image: placeholder,
+        live: "/",
+        github: "/",
+        tags: ["HTML & CSS", "React"]
+    },
 
 
 ]
